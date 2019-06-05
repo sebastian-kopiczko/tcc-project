ab7b9dd95c811923aaf2cc38e491fae298cc1af2
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-06-05 15:03:49 +0200

    Import components, define wheater language, setting state

--- src/App.js
+++ src/App.js
@@ -1,51 +1,73 @@
-import React, { Component } from 'react'
-import Header from './components/Header'
-import './App.css';
-import axios from 'axios';
-import Autocomplete from 'react-google-autocomplete';
+import React, { Component } from "react";
+import axios from "axios";
+
+import AppContainer from "./components/AppContainer";
+import Autocomplete from "react-google-autocomplete";
+import Forecast from "./components/Forecast";
+import "./App.css";
 
 class App extends Component {
-  constructor(){
+  constructor() {
     super();
     this.state = {
-      units: 'si',
-      language: 'pl',
-      apiUrl: 'https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/055a53f5a6b68703fe86456a74f6da17/'
+      isDataReady: false,
+      units: "si",
+      language: "pl",
+      apiUrl:
+        "https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/055a53f5a6b68703fe86456a74f6da17/"
     };
   }
 
   getWheatherData = async () => {
     const { apiUrl, units, language, lat, lng } = this.state;
     try {
-      const response = await axios.get(`${apiUrl}${lat},${lng}?units=${units}&lang=${language}`);
-      console.log(response);
+      const response = await axios.get(
+        `${apiUrl}${lat},${lng}?units=${units}&lang=${language}`
+      );
+      this.setState({ weatherData: response.data, isDataReady: true });
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
-  setCoordinates = (lat, lng, apiCallback) => {
-    this.setState({
-      lat: Number.parseFloat(lat).toPrecision(6), 
-      lng: Number.parseFloat(lng).toPrecision(6)
-    }, () => apiCallback());
-  }
+  setCoordinates = (lat, lng, address, apiCallback) => {
+    this.setState(
+      {
+        lat: Number.parseFloat(lat).toPrecision(6),
+        lng: Number.parseFloat(lng).toPrecision(6),
+        formattedAddress: address
+      },
+      () => apiCallback()
+    );
+  };
 
   render() {
+    const { isDataReady, weatherData, formattedAddress } = this.state;
+    const forecastData = { ...weatherData, formattedAddress };
     return (
-      <div>
-        <Header></Header>
-        <Autocomplete
-          style={{width: '90%'}}
-          onPlaceSelected={place => {
-            const {location} = place.geometry;
-            this.setCoordinates(location.lat(), location.lng(), this.getWheatherData);
-          }}
-          types={['(regions)']}
-          componentRestrictions={{country: "pl"}}
-        />
+      <div className="app">
+        <AppContainer>
+          <Autocomplete
+            style={{ width: "90%" }}
+            types={["(cities)"]}
+            onPlaceSelected={place => {
+              console.log(place);
+              const { formatted_address } = place;
+              const { location } = place.geometry;
+              this.setCoordinates(
+                location.lat(),
+                location.lng(),
+                formatted_address,
+                this.getWheatherData
+              );
+            }}
+          />
+          {isDataReady && Object.entries(weatherData).length !== 0 ? (
+            <Forecast data={forecastData} />
+          ) : null}
+        </AppContainer>
       </div>
-    )
+    );
   }
 }
 export default App;
