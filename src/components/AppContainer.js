eb583a52a5112cf2e302f300af602c6fa73e53dd
Sebastian Kopiczko <sebastian.kopiczko@gmail.com>
2019-06-05 15:07:10 +0200

    Add app container component

---
+++ src/components/AppContainer.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Box from "@material-ui/core/Box";
+
+const AppContainer = props => {
+  return (
+    <div className="app__container">
+      <Box
+        display="flex"
+        p={1}
+        flexDirection="column"
+        bgcolor="background.paper"
+      >
+        {props.children}
+      </Box>
+    </div>
+  );
+};
+export default AppContainer;
